faab12c0e028d47afbf16d9366bd19c43b5e313d
Nabil Babu <nabil.babu20@gmail.com>
2020-11-19 17:42:32 -0500

    Initial commit

---
+++ Assets/_Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿/**
+    SceneChanger.cs
+    Nabil Babu
+    101214336
+    Nov 19th 2020
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneDestination; 
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneDestination);
+    }
+}
