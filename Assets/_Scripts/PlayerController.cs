a95fb937c9d25ed69b0a1eeb3a5391e9afee096f
Nabil Babu <nabil.babu20@gmail.com>
2020-12-12 18:23:52 -0500

    Character Controller Completed, Building Basic Level, UI for character is placed

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerAnimStates
+{
+    IDLE,
+    RUNNING, 
+    JUMPING,
+    DOUBLEJUMPING,
+    ATTACKING,
+}
 public class PlayerController : MonoBehaviour
 {
     [Header("Controls")]
     public Joystick joystick;
     public float joystickHorizontalSensitivity;
     public float horizontalForce;
+    public float verticalForce;
     private Animator m_animator;
     private Rigidbody2D m_rigidBody2D;
+
+    [Header("Player State")]
+    [SerializeField]
+    private bool isGrounded = false;
+    private bool isDoubleJumping = false;  
+    public PlayerAnimStates currentState; 
+    [Header("Player Abilities")] 
+    public int health;
+    public int lives;
 
     // Start is called before the first frame update
     void Start()
@@ -26,23 +44,67 @@ public class PlayerController : MonoBehaviour
 
     void _Move()
     {
-        if(joystick.Horizontal > joystickHorizontalSensitivity)
+        if(joystick.Horizontal > joystickHorizontalSensitivity || Input.GetKey(KeyCode.D))
         {
             // Move Player Right
             m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
             transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            m_animator.SetInteger("runState", 1);
+            if(isGrounded)
+            {
+                currentState = PlayerAnimStates.RUNNING;
+                m_animator.SetInteger("AnimState", (int)currentState);
+            }
         }
-        else if (joystick.Horizontal < - joystickHorizontalSensitivity)
+        else if (joystick.Horizontal < - joystickHorizontalSensitivity || Input.GetKey(KeyCode.A))
         {
             // Move Player Left
             m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
             transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-            m_animator.SetInteger("runState", 1);
+            if(isGrounded)
+            {
+                currentState = PlayerAnimStates.RUNNING;
+                m_animator.SetInteger("AnimState", (int)currentState);
+            }
         }
         else 
         {
-            m_animator.SetInteger("runState", 0);
+            if(isGrounded)
+            {
+                currentState = PlayerAnimStates.IDLE;
+                m_animator.SetInteger("AnimState", (int)currentState);
+            }
         }
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(); 
+        }
+    }
+
+    public void Jump()
+    {
+        if(isGrounded)
+        {
+            m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+            isGrounded = false;
+            // Animate Jump
+            currentState = PlayerAnimStates.JUMPING;
+            m_animator.SetInteger("AnimState", (int)currentState);
+        } 
+        else if(!isGrounded)
+        {
+            if(!isDoubleJumping)
+            {
+                isDoubleJumping = true;
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        isGrounded = true;
+        isDoubleJumping = false; 
     }
 }
